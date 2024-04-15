c9c92d57cd8b4d863f0a329ed2f700b78bc87677
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2024-04-15 20:02:03 +0700

    feat: add 7 task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -140,8 +140,16 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let result = Promise.resolve('');
+  promises.forEach(function ter(item) {
+    result = result.then((res) => {
+      return item.then((value) => {
+        return res + value;
+      });
+    });
+  });
+  return result;
 }
 
 module.exports = {
